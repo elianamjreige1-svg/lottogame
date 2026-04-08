a01ee4f5123312db65a9f4e371e2fa73c5d4de3e
elianamjreige1-svg <eliana.m.jreige1@gmail.com>
2026-04-08 22:05:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const util = require("util");
 
 let winprice = 0;
