92fe421a92ac1fbf84a661777b48d57045448bce
elianamjreige1-svg <eliana.m.jreige1@gmail.com>
2026-05-01 01:04:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get("/start", async (req, res) => {
     } else {
       winprice = 0;
 	}
-	log("winprice = " + winprice);
+	log("winprice = " + results);
   /*  canplay = true;
     tickets = [];
     resultsbyuser = [];
